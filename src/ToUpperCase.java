300b490be8b9685e85895cc46b0274925ee50b24
SHoque <sabrinahoque13@gmail.com>
2019-10-14 13:02:16 +0100

    Added to uppercase java file

---
+++ src/ToUpperCase.java
@@ -0,0 +1,14 @@
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+        String Upper = "hello";
+
+        System.out.println(Upper);
+
+        String result;
+        result = Upper.toUpperCase();
+
+        System.out.println(result);
+
+    }
+}
